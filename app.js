5b36e9be7a2a312f0fc292456a4ffb9204173264
mert679 <64027475+mert679@users.noreply.github.com>
2021-03-30 21:30:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
+let decrease=document.querySelector(".decrease");
+let reset=document.querySelector(".reset");
+let increase=document.querySelector(".increase");
+let board=document.querySelector(".board")
 let count=0;
-// I select button and board
-const board=document.querySelector(".board");
-const btns=document.querySelectorAll(".btn");
-// I created loop with forEach so we provide sustain ;
-btns.forEach(function(btn){
-    btn.addEventListener("click",function(e){
-        const styles=e.currentTarget.classList
-        if (styles.contains("decrease")) {
-            count --
-        }
-        
-        else if (styles.contains("increase")){
-            count++
-        }
-        if (styles.contains("reset")){
-            count=0
-        }
-        if (count>0) {
-            board.style.color="green"
-        }
-        if (count<0) {
-            board.style.color="red"
-        }
-        if (count===0) {
-            board.style.color="yellowgreen"
-        }
-        
-        
-        board.textContent=count;
-    })
+increase.addEventListener("click",()=>{
+    count +=1;
+    board.innerHTML=count
+})
+decrease.addEventListener("click",()=>{
+    count -=1;
+    board.innerHTML=count
+})
+reset.addEventListener("click",()=>{
+    count = 0;
+    board.innerHTML=count
 })
